a9e45d027760bf80c28613625cd224b23aa7118c
visar77 <visarlumi77@gmail.com>
2025-02-27 19:59:16 +0100

    feat: added contour enclosing

--- website/image_stuff/image_conversion.py
+++ website/image_stuff/image_conversion.py
@@ -49,7 +49,7 @@ def process_image(image_path, blur, blockSize, C):
         if np.sum((points[1:, 0] - points[:-1, 0]) * (points[1:, 1] + points[:-1, 1])) >= 0:
             continue
         points_smoothed = smooth_contour(points)
-        point_arrays.append(points_smoothed.tolist())  # Convert to list
+        point_arrays.append(points_smoothed)
 
     all_x = np.concatenate([arr[:, 0] for arr in point_arrays])
     A = np.min(all_x)
@@ -59,5 +59,29 @@ def process_image(image_path, blur, blockSize, C):
     
     for arr in point_arrays:
       arr[:, 0] = fac - arr[:, 0]
-    
+
+    # Interpolate the last point to the first point
+    for i, arr in enumerate(point_arrays):
+        print(f"Processing contour {i+1} with length", len(point_arrays[i]))
+        first_point = arr[0]
+        print(first_point)
+        last_point = arr[-1]
+        print(last_point)
+        # Calculate the distance between the first and last point
+
+        distance = np.sqrt((first_point[0] - last_point[0])**2 + (first_point[1] - last_point[1])**2)
+        print(distance)
+        # If the distance is greater than 0.5, interpolate a new point
+        if distance > 0.5:
+            # Calculate points along the line between the first and last point with step size 10 % of distance
+            new_points = np.linspace(first_point, last_point, int(distance / 0.1))
+            # Insert the new points after the last point
+            point_arrays[i] = np.insert(arr, -1, new_points[1:], axis=0)
+            # Add first point again to close the contour
+            point_arrays[i] = np.insert(point_arrays[i], -1, first_point, axis=0)
+
+        print(f"Done adding points to contour {i+1}, now has length", len(point_arrays[i]))
+
+
+
     return point_arrays
